8b8dec237d4e821c340c653e26e910838a99c24a
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-10-04 22:52:27 -0400

    updated with nouns

---
+++ src/main/java/io/openshift/booster/noun/model/Noun.java
@@ -0,0 +1,45 @@
+package io.openshift.booster.noun.model;
+
+import java.util.Objects;
+
+public class Noun {
+
+	private String noun;
+
+    public Noun() {
+    }
+
+    public Noun(String noun) {
+        this.noun = noun;
+    }
+
+    public String getNoun() {
+        return noun;
+    }
+
+    public Noun noun(String noun) {
+        this.noun = noun;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Noun noun1 = (Noun) o;
+        return Objects.equals(noun, noun1.noun);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(noun);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Noun{");
+        sb.append("noun='").append(noun).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
